4f1f04a367a8477cbd1b07bd4b4f3fcad94ad4a9
Kyle Johnston <kylerjohnston@gmail.com>
2019-12-08 13:35:23 -0500

    Day 4 part 2

--- 4/crack_passwords.rb
+++ 4/crack_passwords.rb
@@ -4,4 +4,5 @@
 require_relative 'cracker'
 
 c = Cracker.new(372037, 905157, 6)
-puts c.passwords.length
+puts "Total passwords (part 1): #{c.passwords.length.to_s}"
+puts "Better passwords (part 2): #{c.better_passwords.length.to_s}"
--- 4/cracker.rb
+++ 4/cracker.rb
@@ -1,14 +1,25 @@
 class Cracker
-  attr_reader :passwords
+  attr_reader :passwords, :better_passwords
 
   def initialize(range_min, range_max, length)
     @range_min = range_min
     @range_max = range_max
     @length = length
     @passwords = find_passwords
+    @better_passwords = find_better_passwords
   end
 
   private
+
+  def find_better_passwords
+    matches = []
+    @passwords.each do |pw|
+      if pw.is_better_match?
+        matches << pw
+      end
+    end
+    return matches
+  end
 
   def find_passwords
     all_combinations = *(@range_min..@range_max)
@@ -28,11 +39,22 @@ class Password
     @password = password.to_s
     @pass_array = @password.split('')
     @length = length
+    @dupes = []
   end
 
   def is_match?
     if self.is_right_length? and self.has_dupes? and self.is_increasing?
       return true
+    end
+    return nil
+  end
+
+  # Day 4 Part 2
+  def is_better_match?
+    @dupes.each do |x|
+      if @password.count(x) == 2
+        return true
+      end
     end
     return nil
   end
@@ -51,12 +73,18 @@ class Password
   # Condition 2 satisfied in Cracker.find_passwords
   # Condition 3
   def has_dupes?
+    dupes = nil
     for i in 0..@pass_array.length - 2
       if @pass_array[i] == @pass_array[i + 1]
-        return true
+        dupes = true
+        @dupes << @pass_array[i]
       end
     end
-    return nil
+    if dupes
+      return true
+    else
+      return nil
+    end
   end
 
   # Condition 4
